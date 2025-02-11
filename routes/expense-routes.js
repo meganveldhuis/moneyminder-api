db074192d10658fbc283adfdf832db7bcf860feb
lovedatsnow <megan.veldhuis@gmail.com>
2025-02-10 17:28:16 -0700

    set up expense and income controllers and routes

---
+++ routes/expense-routes.js
@@ -0,0 +1,6 @@
+import express from "express";
+const router = express.Router();
+
+import * as expenseController from "../controllers/expense-controller.js";
+
+export default router;
---
+++ routes/income-routes.js
@@ -0,0 +1,6 @@
+import express from "express";
+const router = express.Router();
+
+import * as incomeController from "../controllers/income-controller.js";
+
+export default router;
