3afbc06230e2096f97c071f724cb17419e0c4eb3
lovedatsnow <megan.veldhuis@gmail.com>
2025-02-13 11:24:09 -0700

    add expense endpoints

---
+++ controllers/expense-controller.js
@@ -0,0 +1,264 @@
+import initKnex from "knex";
+import configuration from "../knexfile.js";
+const knex = initKnex(configuration);
+
+/* -------------------------------------------------------------------------- */
+/*                             Reusable Functions                             */
+/* -------------------------------------------------------------------------- */
+function isValidDate(d) {
+  return d instanceof Date && !isNaN(d);
+}
+
+async function doesCategoryExist(category_id) {
+  let status = 200;
+  let message = ``;
+  let categoryData = [];
+  try {
+    categoryData = await knex("categories").where({
+      id: category_id,
+    });
+    if (!categoryData || categoryData.length === 0) {
+      status = 404;
+      message = `No category found with ID ${category_id}.`;
+      console.log(`No category found with ID ${category_id}.`);
+    }
+  } catch (error) {
+    status = 404;
+    message = `Error: could not find category with ID ${category_id}`;
+    console.log(
+      `Error: could not find category with ID ${category_id} ${error}`
+    );
+  }
+  return [status, message, categoryData[0]];
+}
+
+async function doesCurrencyExist(currency_id) {
+  let status = 200;
+  let message = ``;
+  let currencyData;
+  try {
+    currencyData = await knex("currencies").where({
+      id: currency_id,
+    });
+    if (!currencyData || currencyData.length === 0) {
+      status = 404;
+      message = `No currency found with ID ${currency_id}.`;
+      console.log(`No currency found with ID ${currency_id}.`);
+    }
+  } catch (error) {
+    status = 404;
+    message = `Error: could not find currency with ID ${currency_id}`;
+    console.log(
+      `Error: could not find currency with ID ${currency_id} ${error}`
+    );
+  }
+  return [status, message, currencyData];
+}
+
+/* -------------------------------------------------------------------------- */
+/*                              Expenses Endpoints                            */
+/* -------------------------------------------------------------------------- */
+
+/*
+ * [x] `get` all expense records
+ * [x] `get` expense by id
+ * [x] `post` new expense record
+ * [x] `patch` edit expense record
+ * [x] `delete` expense record
+ */
+
+export async function getAllExpenses(req, res) {
+  console.log("get all expenses");
+  let query = req.query.search || "";
+  try {
+    let queryBuilder = knex("expenses").select(
+      "expenses.id",
+      "expenses.date",
+      "expenses.created_at",
+      "expenses.updated_at",
+      "expenses.name",
+      "expenses.amount",
+      "categories.category_name",
+      "currencies.code",
+      "trips.trip_name"
+    );
+    queryBuilder = queryBuilder
+      .join("categories", "categories.id", "category_id")
+      .join("currencies", "currencies.id", "currency_id")
+      .leftJoin("trips", "trips.id", "trip_id")
+      .whereILike("expenses.name", `%${query}%`);
+    const data = await queryBuilder;
+
+    res.send(data);
+  } catch (error) {
+    console.log(`Error getting all expense records: ${error}`);
+    res.status(500).send(`Error getting all expense records`);
+  }
+}
+
+export async function getSingleExpenseRecord(req, res) {
+  try {
+    let queryBuilder = knex("expenses").select(
+      "expenses.id",
+      "expenses.date",
+      "expenses.created_at",
+      "expenses.updated_at",
+      "expenses.name",
+      "expenses.amount",
+      "categories.category_name",
+      "currencies.code as currency_code",
+      "trips.trip_name"
+    );
+    queryBuilder = queryBuilder
+      .join("categories", "categories.id", "category_id")
+      .join("currencies", "currencies.id", "currency_id")
+      .leftJoin("trips", "trips.id", "trip_id")
+      .where({ "expenses.id": req.params.id });
+    const data = await queryBuilder;
+    if (data.length < 1) {
+      return res.status(404).send(`No expense record with id ${req.params.id}`);
+    }
+
+    res.send(data[0]);
+  } catch (error) {
+    console.log(
+      `Error getting expense record with id ${req.params.id}: ${error}`
+    );
+    res
+      .status(500)
+      .send(`Error getting expense record with id ${req.params.id}`);
+  }
+}
+
+export async function addExpenseRecord(req, res) {
+  /* ----------------------------- Validate Inputs ---------------------------- */
+  let newRecord = req.body;
+  if (
+    !newRecord.date ||
+    !newRecord.name ||
+    !newRecord.amount ||
+    !newRecord.category_id ||
+    !newRecord.currency_id
+  ) {
+    return res.status(400).json({
+      message:
+        "Missing properties in the request body. Please include date, name, amount, category_id, and currency_id",
+    });
+  }
+  newRecord.date = new Date(newRecord.date);
+  if (!isValidDate(newRecord.date)) {
+    return res.status(400).send(`Error: ${newRecord.date} is not a valid date`);
+  }
+  //check if category exists
+  const doesCategory = await doesCategoryExist(newRecord.category_id);
+  let [status, message, categoryData] = doesCategory;
+  if (status !== 200) {
+    return res.status(status).send(message);
+  }
+
+  //check if currency exists
+  const doesCurrency = await doesCurrencyExist(newRecord.currency_id);
+  let currencyData;
+  [status, message, currencyData] = doesCurrency;
+  if (status !== 200) {
+    return res.status(status).send(message);
+  }
+
+  /* --------------------- Add new Record to Expenses Table --------------------- */
+  try {
+    const newRecordID = await knex("expenses").insert(newRecord);
+    res.status(201).json(newRecordID);
+  } catch (error) {
+    console.log(`Error adding new expense record: ${error}`);
+    res.status(500).send(`Error adding new expense record`);
+  }
+}
+
+export async function editExpenseRecord(req, res) {
+  /* ----------------------------- Validate Inputs ---------------------------- */
+  //TODO: DOESNT WORK WHEN CURRENCY ID or OTHER INPUTS ARE UNDEFINED:(
+  const { id } = req.params;
+  let edits = {};
+  let status, message, categoryData, currencyData;
+  console.log(`currencyid: ${req.body.currency_id}`);
+  switch (true) {
+    case req.body.date !== undefined:
+      console.log("ran date");
+      edits.date = new Date(req.body.date);
+
+    case req.body.name !== undefined:
+      edits.name = req.body.name;
+
+    case req.body.amount !== undefined:
+      edits.amount = Number(req.body.amount);
+
+    case req.body.category_id !== undefined:
+      //check if category exists
+      const doesCategory = await doesCategoryExist(req.body.category_id);
+      [status, message, categoryData] = doesCategory;
+      if (status !== 200) {
+        return res.status(status).send(message);
+      }
+      edits.category_id = req.body.category_id;
+
+    case req.body.currency_id !== undefined:
+      //check if currency exists
+      const doesCurrency = await doesCurrencyExist(req.body.currency_id);
+      [status, message, currencyData] = doesCurrency;
+      if (status !== 200) {
+        return res.status(status).send(message);
+      }
+      edits.currency_id = req.body.currency_id;
+    default:
+  }
+
+  /* --------------------------- Check record exists -------------------------- */
+  try {
+    const expenseRecord = await knex("expenses").where({ id: id }).first();
+    if (!expenseRecord) {
+      return res.status(404).send(`Expense record not found with id ${id}`);
+    }
+  } catch (error) {
+    console.log(`Expense record not found with id ${id}: ${error}`);
+    return res.status(404).send(`Expense record not found with id ${id}`);
+  }
+
+  /* -------------------------- Edit Record in Table -------------------------- */
+  try {
+    const numberRowsChanged = await knex("expenses")
+      .where({ id: id })
+      .update(edits);
+    if (numberRowsChanged < 1) {
+      return res.status(500).send(`Error occured. Expense record not updated`);
+    }
+    const editedRecord = await getSingleExpenseRecord(req, res);
+  } catch (error) {
+    console.log(`Error editing expense record: ${error}`);
+    return res.status(500).send(`Error editing expense record`);
+  }
+}
+
+export async function deleteExpenseRecord(req, res) {
+  const { id } = req.params;
+  try {
+    // Check if expenses record exists before deleting
+    const existingRecord = await knex("expenses").where({ id }).first();
+    if (!existingRecord) {
+      return res
+        .status(404)
+        .json({ message: `No expense record found with id: ${id}` });
+    }
+
+    // Delete the record
+    await knex("expenses").where({ id }).del();
+
+    return res.status(200).json({
+      message: `expense record with id ${id} deleted successfully`,
+    });
+  } catch (error) {
+    console.error(`Error deleting record with id ${id}:`, error);
+    return res.status(500).json({
+      message: `Error deleting record with id: ${id}`,
+    });
+  }
+}
--- routes/expense-routes.js
+++ routes/expense-routes.js
@@ -2,5 +2,14 @@ import express from "express";
 const router = express.Router();
 
 import * as expenseController from "../controllers/expense-controller.js";
+router
+  .route("/")
+  .get(expenseController.getAllExpenses)
+  .post(expenseController.addExpenseRecord);
 
+router
+  .route("/:id")
+  .get(expenseController.getSingleExpenseRecord)
+  .patch(expenseController.editExpenseRecord)
+  .delete(expenseController.deleteExpenseRecord);
 export default router;
