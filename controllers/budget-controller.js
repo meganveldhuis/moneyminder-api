423897d67f5d82ad89cdbb8610c7bf13f7d932df
Megan Veldhuis <megan.veldhuis@gmail.com>
2025-02-22 20:55:04 -0700

    add option to add new budget line and add / edit category

--- controllers/budget-controller.js
+++ controllers/budget-controller.js
@@ -4,7 +4,6 @@ const knex = initKnex(configuration);
 
 // routes:
 // [x] GET all budget lines
-// [ ] GET combined for summary page's cards : budget amount, actual amount, and difference by category_name
 // [x] GET budget line by category id
 
 export async function getAllBudget(req, res) {
@@ -41,5 +40,74 @@ export async function getBudgetByCategory(req, res) {
     return res
       .status(500)
       .send(`Error getting budget line with category id ${category_id}`);
+  }
+}
+
+export async function addBudgetAndCategory(req, res) {
+  console.log(req.body);
+  let newCategory = {
+    category_name: req.body.category_name,
+    description: req.body.description,
+    is_income: req.body.is_income || true,
+  };
+  let newBudget = {
+    amount: req.body.amount,
+    note: req.body.note,
+    is_per_year: req.body.is_per_year || false,
+  };
+  if (
+    !newCategory.category_name ||
+    !newCategory.description ||
+    !newBudget.amount
+  ) {
+    return res.status(400).json({
+      message:
+        "Missing properties in the request body. Please include category_name, description, and amount",
+    });
+  }
+  /* --------------------- Check Category isn't already in Categories Table --------------------- */
+  let isNewCategory = false;
+  let newCategoryID;
+  let categoryID = await knex("categories")
+    .select("id")
+    .where({
+      category_name: newCategory.category_name,
+    })
+    .first();
+
+  if (categoryID) {
+    //category with that name already exists, so edit existing category and don't create a new one
+    const numberRowsChanged = await knex("categories")
+      .where({ id: categoryID.id })
+      .update(newCategory);
+  } else {
+    /* --------------------- Add new Category to Categories Table --------------------- */
+
+    try {
+      newCategoryID = await knex("categories").insert(newCategory);
+      if (newCategoryID.length === 0) {
+        return res.status(500).send(`Error adding new category`);
+      }
+      isNewCategory = true;
+    } catch (error) {
+      console.log(`Error adding new category: ${error}`);
+      return res.status(500).send(`Error adding new category`);
+    }
+  }
+
+  /* ---------------------Add new Budget Line to Budget Table--------------------- */
+
+  try {
+    if (isNewCategory) {
+      newBudget.category_id = newCategoryID[0];
+    } else {
+      newBudget.category_id = categoryID.id;
+    }
+
+    const newBudgetID = await knex("budget").insert(newBudget);
+    res.status(201).json(newBudgetID);
+  } catch (error) {
+    console.log(`Error adding new budget: ${error}`);
+    res.status(500).send(`Error adding new budget`);
   }
 }
--- routes/budget-routes.js
+++ routes/budget-routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 import * as budgetContoller from "../controllers/budget-controller.js";
 router.route("/").get(budgetContoller.getAllBudget);
+router.route("/category").put(budgetContoller.addBudgetAndCategory);
 
 router.route("/:id").get(budgetContoller.getBudgetByCategory);
 
